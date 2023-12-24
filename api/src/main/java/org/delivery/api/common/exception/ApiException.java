65922679ef88258acbdfdb6ccff179e0446c41d3
borussia444423 <jung78978978@naver.com>
2023-12-24 21:01:07 +0900

    JWT 토큰 서비스 로직 적용

--- api/src/main/java/org/delivery/api/common/exception/ApiException.java
+++ api/src/main/java/org/delivery/api/common/exception/ApiException.java
@@ -9,19 +9,25 @@ public class ApiException extends RuntimeException implements ApiExceptionIfs {
 	private final ErrorCodeIfs errorCodeIfs;
 	private final String errorDescription;
 
-	public ApiException(ErrorCodeIfs errorCodeIfs) {
+	public ApiException(ErrorCodeIfs errorCodeIfs){
 		super(errorCodeIfs.getDescription());
 		this.errorCodeIfs = errorCodeIfs;
 		this.errorDescription = errorCodeIfs.getDescription();
 	}
 
-	public ApiException(ErrorCodeIfs errorCodeIfs, String errorDescription) {
+	public ApiException(ErrorCodeIfs errorCodeIfs, String errorDescription){
 		super(errorDescription);
 		this.errorCodeIfs = errorCodeIfs;
 		this.errorDescription = errorDescription;
 	}
 
-	public ApiException(ErrorCodeIfs errorCodeIfs, Throwable tx, String errorDescription) {
+	public ApiException(ErrorCodeIfs errorCodeIfs, Throwable tx){
+		super(tx);
+		this.errorCodeIfs = errorCodeIfs;
+		this.errorDescription = errorCodeIfs.getDescription();
+	}
+
+	public ApiException(ErrorCodeIfs errorCodeIfs, Throwable tx, String errorDescription){
 		super(tx);
 		this.errorCodeIfs = errorCodeIfs;
 		this.errorDescription = errorDescription;
